a43bcfc35e616b79666f53c5353ca0c7e8a27ec6
Urban Kovač <urban.kovac@kalmia.si>
2024-02-12 13:38:41 +0100

    FE fixes

--- frontend/plugins/use-wagmi.ts
+++ frontend/plugins/use-wagmi.ts
@@ -1,17 +1,16 @@
 import { UseWagmiPlugin, createConfig } from 'use-wagmi';
-import { astar, moonbaseAlpha, moonbeam } from 'use-wagmi/chains';
+import { moonbaseAlpha, moonbeam } from 'use-wagmi/chains';
 import { MetaMaskConnector } from 'use-wagmi/connectors/metaMask';
 import { CoinbaseWalletConnector } from 'use-wagmi/connectors/coinbaseWallet';
 import { WalletConnectConnector } from 'use-wagmi/connectors/walletConnect';
 import { createPublicClient, http } from 'viem';
-import { Chains } from 'lib/values/general.values';
+import { Chains } from '~/lib/values/general.values';
 
 export default defineNuxtPlugin(nuxtApp => {
   const nuxtConfig = useRuntimeConfig();
   const chainId = nuxtConfig.public.CHAIN_ID;
 
-  const chain =
-    chainId === Chains.MOONBASE ? moonbaseAlpha : chainId === Chains.ASTAR ? astar : moonbeam;
+  const chain = chainId === Chains.MOONBASE ? moonbaseAlpha : moonbeam;
   const chains = [chain];
 
   const config = createConfig({
