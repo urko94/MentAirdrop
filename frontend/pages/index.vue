31c508e19f7d5a8381c1685704bdb40eb2af0d68
ZanJereb <zan_jereb@hotmail.com>
2024-02-13 13:36:49 +0100

    Update index.vue

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -6,7 +6,6 @@ useHead({
 </script>
 
 <template>
-  <!-- #region UNSTOPPABLE NFTS -->
   <div class="my-20 flex flex-col gap-x-12 gap-y-8 md:flex-row lg:my-32">
     <div class="max-w-2xl md:pr-12">
       <span class="mb-2 inline-block text-xs font-bold uppercase tracking-[0.2em] text-info">The Shape of MENT to Come</span>
